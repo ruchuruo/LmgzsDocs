ccc1e5a02d46117775d7dae682419b301c89f944
ruchuruo <ruchuruo@DESKTOP-I0JD1J7>
2024-11-13 22:50:48 +0800

    2024年11月13日 添加字体 apaxy开启lightgallery 成员介绍添加

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,9 +5,10 @@ import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 
 //引入自定义的样式
 import "./styles/home-page.css";              // 主页样式
-import "./styles/navBar.css";                 // 导航栏样式
+import "./styles/class-VPNavBar.css";         // 导航栏样式
 import "./styles/element-body-background.css" // 元素 body样式 背景
 import "./styles/id-app-background.css"       // id app的背景
+import "./styles/fonts.css"                   // 字体
 
 
 
